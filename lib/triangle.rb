61b607aaf2acd03e17f1dd101d5ad9a2d9beb224
Austin Powell <austinpowell58@gmail.com>
2018-08-25 15:33:44 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
-  # write code here
+  attr_accessor :firstside, :secondside, :thirdside
+  
+  
+  def initialize(firstside:, secondside:, thirdside:)
+    @firstside = firstside
+    @secondside = secondside
+    @thirdside = thirdside
+  end 
+  
+  def kind
+  
 end
